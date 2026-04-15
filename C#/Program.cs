c54d770a3285cd68a58e05f57be5980488679341
user <user@WIN-4KGTNI4DDSK>
2026-04-15 19:22:15 +0300

    Add project files.

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,9 +1,80 @@
-﻿namespace _13._04
+﻿//#define Main
+using System.Linq;
+using System;
+
+#if Main 
+namespace _13._04
 {
     internal class Program
     {
+        static int Sqea(int x = 0) => x * x;
+        /*
+        {
+            return x * x;
+        }
+        */
+
+        static void Temp(ref int x)
+        {
+            x = 4;
+        }
+
+        static void Mas(int[] mas, int min = 0, int max = int.MaxValue)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < mas.Length; ++i)
+            {
+                mas[i] = rand.Next(min, max);
+            }
+        }
+
+        static void Mas(string[] mas, string[] items)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < mas.Length; ++i)
+            {
+                mas[i] = items[rand.Next(0, items.Length)];
+            }
+        }
+
+        static void Mas(string[] mas, int minLength = 0, int maxLength = 255)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < mas.Length; ++i)
+            {
+                mas[i] = "";
+                int randLength = rand.Next(minLength, maxLength);
+                for (int j = 0; j < randLength; ++j)
+                {
+                    mas[i] += char.ConvertFromUtf32(rand.Next(0, 255));
+                }
+            }
+        }
+
+        static void PrintMas(int[] mas)
+        {
+            foreach (int i in mas)
+            {
+                Console.Write($"{i}\t");
+            }
+        }
         static void Main(string[] args)
         {
+            {
+                int[] mas = new int[10];
+                Mas(mas, -10, 50);
+                PrintMas(mas);
+                Console.WriteLine();
+            }
+
+            {
+                int x = 509;
+                // передача значений по ссылке
+                Temp(ref x);
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+
             /*
              * Числовые
              *  1. Целые
@@ -20,7 +91,7 @@
              *      float       // 4 байта
              *      double      // 8 байт
              */
-            {
+            /*{
                 float fv = 3.2f;
                 double dv = 3.2;
 
@@ -32,7 +103,7 @@
                 ushort usv = 2;
                 uint uiv = 2;
                 ulong ulv = 2;
-            }
+            }*/
 
             /**
              * 1. Логические
@@ -91,12 +162,12 @@
 
             // циклы
             {
-                for(int i = 0; i < 10; ++i)
+                for (int i = 0; i < 10; ++i)
                 {
                     Console.WriteLine($"[{i}]");
                 }
 
-                while(false) { }
+                while (false) { }
 
                 do { } while (false);
             }
@@ -113,15 +184,15 @@
                 Console.WriteLine($"Sum = ${mas.Sum()}");
                 Console.WriteLine($"Avg = ${mas.Average()}");
 
-                foreach(int v in mas)
+                foreach (int v in mas)
                 {
                     Console.WriteLine(v);
                 }
 
                 int[,] sqeaMatrix = new int[5, 2];
-                for(int i = 0; i < sqeaMatrix.GetLength(0); ++i)
+                for (int i = 0; i < sqeaMatrix.GetLength(0); ++i)
                 {
-                    for(int j = 0; j < sqeaMatrix.GetLength(1); ++j)
+                    for (int j = 0; j < sqeaMatrix.GetLength(1); ++j)
                     {
                         sqeaMatrix[i, j] = i * j;
                     }
@@ -137,14 +208,14 @@
                 }
 
                 int[][] stepsMatrix = new int[5][];
-                for(int i = 0; i < stepsMatrix.Length; ++i)
+                for (int i = 0; i < stepsMatrix.Length; ++i)
                 {
                     stepsMatrix[i] = new int[i + 1];
                 }
 
-                for(int i = 0; i < stepsMatrix.Length; ++i)
+                for (int i = 0; i < stepsMatrix.Length; ++i)
                 {
-                    for(int j = 0; j < stepsMatrix[i].Length; ++j)
+                    for (int j = 0; j < stepsMatrix[i].Length; ++j)
                     {
                         stepsMatrix[i][j] = i * j;
                     }
@@ -168,7 +239,41 @@
                         этого же массива
              * 
              */
+            // Ввод размера массива
+            Console.Write("Введите размер массива: ");
+            int s = int.Parse(Console.ReadLine());
+
+            // Создание и заполнение массива
+            double[] array = new double[s];
+            for (int i = 0; i < s; i++)
+            {
+                Console.Write($"Введите элемент [{i}]: ");
+                array[i] = double.Parse(Console.ReadLine());
+            }
+
+            // Вычисление среднего значения массива
+            double sum = 0;
+            foreach (double num in array)
+            {
+                sum += num;
+            }
+            double average = sum / s;
+
+            // Подсчет количества элементов, которые больше среднего
+            int countAboveAverage = 0;
+            foreach (double num in array)
+            {
+                if (num > average)
+                {
+                    countAboveAverage++;
+                }
+            }
+
+            // Вывод результата
+            Console.WriteLine($"\nСреднее значение массива: {average}");
+            Console.WriteLine($"Количество элементов, которые больше среднего: {countAboveAverage}");
         }
-        
+
     }
 }
+#endif //Main
---
+++ C#/main.cs
@@ -0,0 +1,168 @@
+﻿//#define Main
+using System;
+
+#if Main 
+namespace _15._04
+{
+    class NameClass
+    {
+        public int x;
+        protected string y;
+        private float t;
+        /*private*/
+        double g;
+    }
+
+    class Point
+    {
+        int x, y;
+
+        // Синтаксический сахар - Специальная обертка над(для) языком, которая позволяет более удобно/компактно писать код
+        // Синтаксис свойства C# - специальный синтаксис для методов get/set
+        /*
+         * ASSECC_LEVEL TYPE NAME_PROPERTY {
+         *      ASSECC_LEVEL get {
+         *          ...
+         *      }
+         *      
+         *      ASSECC_LEVEL set {
+         *          ...
+         *      }
+         * }
+         */
+
+        public int X
+        {
+            // public int getX() {return x;}
+            get
+            {
+                return x;
+            }
+            /*protected*/
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y
+        {
+            get => y;
+            /*protected*/
+            set => y = value;
+        }
+
+        public double Distanse(Point other)
+        {
+            return Math.Sqrt(Math.Pow(x - other.x, 2) + Math.Pow(y - other.y, 2));
+        }
+
+        public int getX() => x;
+        public int getY()
+        {
+            return y;
+        }
+    }
+
+    /**
+     *  Класс квадрат {Ширина, Высота} через свойства!
+     *      Методы расчёта периметра и площади
+     *      Создать 2 экземпляра и сравнить их по площади
+     */
+    class Square 
+    {
+        double h, w;
+
+        public double H
+        {
+            // public int getX() {return x;}
+            get
+            {
+                return h;
+            }
+            /*protected*/
+            set
+            {
+                h = value;
+            }
+        }
+        public double W
+        {
+            get
+            {
+                return w;
+            }
+            set
+            {
+                w = value;
+            }
+        }
+
+        public double P() 
+        {
+            return 2 * w + 2 * h;
+        }
+        public double S()
+        {
+            return w * h;
+        }
+    }
+
+
+    internal class Program
+    {
+
+        static void Temp(NameClass name)
+        {
+            name.x = 111;
+        }
+
+        static double GetDistanse(Point a, Point b)
+        {
+            return Math.Sqrt(
+                Math.Pow(a.X - b.X, 2) +
+                Math.Pow(a.Y - b.Y, 2)
+            );
+            /*
+            return Math.Sqrt(
+                Math.Pow(a.getX() - b.getX(), 2) + 
+                Math.Pow(a.getY() - b.getY(), 2)
+            );
+            */
+        }
+
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.X = 10; p1.Y = 20;
+            Point p2 = new Point { X = 20, Y = 10 };
+
+            Console.WriteLine($"Расстояние = {GetDistanse(p1, p2)}");
+
+            // Создана переменная типа NameClass, но не проинициплизовая
+            // Все объекты классов, всегда, являются ссылками
+            NameClass nameClass;
+
+            nameClass = new NameClass();
+            nameClass.x = -2;
+
+            Console.WriteLine(nameClass.x);
+            Temp(nameClass);
+            Console.WriteLine(nameClass.x);
+
+            //Square
+            Square sq1 = new Square { H = 10, W = 20 };
+            Square sq2 = new Square { H = 11, W = 7 };
+
+            double S_1 = sq1.S();
+            double S_2 = sq2.S();
+            if (S_1 > S_2)
+                Console.WriteLine($"Площадь первого ({S_1}) больше");
+            else if (S_1 < S_2)
+                Console.WriteLine($"Площадь второго ({S_2}) больше");
+            else
+                Console.WriteLine("Площади равны");
+        }
+    }
+}
+#endif //Main
