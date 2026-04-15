8a4542b4530e94655edf150a3e97ae8233d593fb
user <user@WIN-4KGTNI4DDSK>
2026-04-15 20:50:30 +0300

    Add project files.

--- C#/main.cs
+++ C#/main.cs
@@ -4,6 +4,24 @@ using System;
 #if Main 
 namespace _15._04
 {
+    // interface - описания возможностей класса
+
+    /*
+     * interface INameInterface {
+     * }
+     */
+    interface IMoveble2D
+    {
+        // Модификаторы доступа не прописываются, т.к они тут по умолчанию все должны быть public
+        // реализацию так же дать нельзя
+        void Move(int x, int y);
+    }
+
+    interface IMoveble3D
+    {
+        void Move(int x, int y, int z);
+    }
+
     class NameClass
     {
         public int x;
@@ -11,9 +29,23 @@ namespace _15._04
         private float t;
         /*private*/
         double g;
+
+        public const int version = 2;
     }
 
-    class Point
+    class Circle
+    {
+        // readonly - модификатор, который обозначает поле, как доступное только на чтение
+        // НО один раз ему можно указать значение - только в конструкторе
+        public readonly int r;
+
+        public Circle(int r)
+        {
+            this.r = r;
+        }
+    }
+
+    class Point2D : IMoveble2D
     {
         int x, y;
 
@@ -30,6 +62,13 @@ namespace _15._04
          *      }
          * }
          */
+
+        public Point2D() { }
+        public Point2D(int x, int y)
+        {
+            this.y = y;
+            this.x = x;
+        }
 
         public int X
         {
@@ -52,7 +91,7 @@ namespace _15._04
             set => y = value;
         }
 
-        public double Distanse(Point other)
+        public virtual double Distanse(Point2D other)
         {
             return Math.Sqrt(Math.Pow(x - other.x, 2) + Math.Pow(y - other.y, 2));
         }
@@ -62,6 +101,60 @@ namespace _15._04
         {
             return y;
         }
+
+        public void Move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Point3D : Point2D, IMoveble3D
+    {
+        int z;
+
+        public Point3D() { }
+
+        public Point3D(int x, int y, int z) : base(x, y)
+        {
+            this.z = z;
+        }
+
+        public int Z
+        {
+            get => z; set => z = value;
+        }
+
+        public void Move(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            this.z = z;
+        }
+    }
+
+    partial class TempPartial
+    {
+        int field1, field2, field3;
+    }
+
+    partial class TempPartial
+    {
+        public int Sum()
+        {
+            return field1 + field2 + field3;
+        }
+    }
+
+    abstract class TempVirtual
+    {
+        // Виртуальный метод объязан иметь своё собственное опредение
+        public virtual void Virtual() { }
+
+        // Абстрактный метод не должен иметь своё определение,
+        // Но так же, если в классе есть хотя бы один абстрактный метод, то весь класс так же должен быть
+        // отмечен как абстрактный
+        public abstract void Abstract();
     }
 
     /**
@@ -69,18 +162,16 @@ namespace _15._04
      *      Методы расчёта периметра и площади
      *      Создать 2 экземпляра и сравнить их по площади
      */
-    class Square 
+    class Square
     {
         double h, w;
 
         public double H
         {
-            // public int getX() {return x;}
             get
             {
                 return h;
             }
-            /*protected*/
             set
             {
                 h = value;
@@ -98,7 +189,7 @@ namespace _15._04
             }
         }
 
-        public double P() 
+        public double P()
         {
             return 2 * w + 2 * h;
         }
@@ -108,7 +199,6 @@ namespace _15._04
         }
     }
 
-
     internal class Program
     {
 
@@ -117,7 +207,7 @@ namespace _15._04
             name.x = 111;
         }
 
-        static double GetDistanse(Point a, Point b)
+        static double GetDistanse(Point2D a, Point2D b)
         {
             return Math.Sqrt(
                 Math.Pow(a.X - b.X, 2) +
@@ -133,9 +223,17 @@ namespace _15._04
 
         static void Main(string[] args)
         {
-            Point p1 = new Point();
+            Point3D point3D = new Point3D { X = 1, Y = 3, Z = 2 };
+            Console.WriteLine($"X = {point3D.X}, Y = {point3D.Y}, Z = {point3D.Z}");
+
+            Circle circle = new Circle(2);
+
+            // Ошибка - работа с полей, обозначенным как readonly, на запись
+            // circle.r = 2;
+
+            Point2D p1 = new Point2D();
             p1.X = 10; p1.Y = 20;
-            Point p2 = new Point { X = 20, Y = 10 };
+            Point2D p2 = new Point2D { X = 20, Y = 10 };
 
             Console.WriteLine($"Расстояние = {GetDistanse(p1, p2)}");
 
@@ -165,4 +263,5 @@ namespace _15._04
         }
     }
 }
+
 #endif //Main
